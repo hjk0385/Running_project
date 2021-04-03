0731f56f1077e8ae10211eb29968375b692262b3
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-04-03 12:17:37 +0900

    NaverMapBranchCommit

---
+++ app/src/main/java/com/trainer/courserunner/CourseGuideActivity.java
@@ -0,0 +1,21 @@
+package com.trainer.courserunner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentManager;
+
+import com.naver.maps.map.MapFragment;
+import com.trainer.courserunner.courseguider.CourseOverseer;
+import com.trainer.courserunner.courseguider.MapDrawer;
+
+public class CourseGuideActivity extends NavermapActivity{
+    CourseOverseer courseOverseer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        courseOverseer=new CourseOverseer((MapDrawer)this);
+        //thread start
+
+
+    }
+}
---
+++ app/src/main/java/com/trainer/courserunner/CourseTestActivity.java
@@ -0,0 +1,13 @@
+package com.trainer.courserunner;
+
+import android.os.Bundle;
+
+public class CourseTestActivity extends NavermapActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
