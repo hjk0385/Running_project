54ffdbf0f3f2042ec3246b6a2840742909b8481c
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-04-15 13:21:17 +0900

    commit

--- app/src/main/java/com/trainer/courserunner/course/CourseGuideActivity.java
+++ app/src/main/java/com/trainer/courserunner/course/CourseGuideActivity.java
@@ -1,6 +1,7 @@
 package com.trainer.courserunner.course;
 
 import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteBindOrColumnIndexOutOfRangeException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
@@ -8,13 +9,17 @@ import android.os.Bundle;
 
 import com.trainer.courserunner.managedata.AssetLoader;
 import com.trainer.courserunner.maps.NavermapLocationActivity;
+import com.trainer.courserunner.scopetype.ScopeDotAddress;
+import com.trainer.courserunner.scopetype.ScopeDotLocation;
+import com.trainer.courserunner.scopetype.ScopeDotsImage;
+import com.trainer.courserunner.scopetype.ScopeDotsMap;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 
 public class CourseGuideActivity extends NavermapLocationActivity {
-    CourseSuggesterDrawer courseSuggesterDrawer;
     CourseOverseer courseOverseer;
 
     @Override
@@ -26,20 +31,25 @@ public class CourseGuideActivity extends NavermapLocationActivity {
 
     @Override
     public void onMapReady() {
+        //test code
         double startx = 126.7687037;
         double starty = 37.4916138;
         double endx = 126.779899;
         double endy = 37.506515;
 
-        CourseSuggesterDrawer=new CourseSuggesterDrawer(AssetLoader(this,"testbitmap1.png"),)
+        ScopeDotsImage image=new ScopeDotsImage(AssetLoader.loadImage(this,"testbitmap1.png"));
+        ScopeDotsMap maps=new ScopeDotsMap(startx,starty,endx,endy);
+        ScopeDotLocation currentLocation=new ScopeDotLocation(startx,starty,endx,endy,startx,endy);
 
-        CourseSuggester courseSuggester = new CourseSuggester(bitmap, startx, starty, endx, endy);
-        courseOverseer = new CourseOverseer(this, courseSuggester.suggestPath(), startx, starty, endx, endy);
+        List<ScopeDotAddress> course=CourseSuggester.suggestPath(image,maps,currentLocation);
+
+        courseOverseer = new CourseOverseer(this);
+        courseOverseer.startOverseer(course,currentLocation);
     }
 
     @Override
     public void onLocationChangeListener(Location location) {
         super.onLocationChangeListener(location);
-        courseOverseer.refresh(userLongitude, userLatitude);
+        courseOverseer.refreshOversight(userLongitude, userLatitude);
     }
 }
--- app/src/main/java/com/trainer/courserunner/course/CourseOverseer.java
+++ app/src/main/java/com/trainer/courserunner/course/CourseOverseer.java
@@ -1,107 +1,48 @@
 package com.trainer.courserunner.course;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.trainer.courserunner.maps.MapDrawer;
 import com.trainer.courserunner.scopetype.ScopeDotAddress;
+import com.trainer.courserunner.scopetype.ScopeDotLocation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CourseOverseer {
-    //drawer
+    //Drawer
     MapDrawer mapDrawer;
-    //Info
-    double startX;
-    double startY;
-    double endX;
-    double endY;
-    //Path
-    int pathNumber;
-    List<ScopeDotAddress> passedPath;
-    List<ScopeDotAddress> remainPath;
-    //Overlay
-    Object overlayPassedPath;
-    Object overlayRemainPath;
-
-    public CourseOverseer(MapDrawer mapDrawer, List<ScopeDotAddress> addressList, double startX, double startY, double endX, double endY) {
-        //drawer
-        this.mapDrawer = mapDrawer;
-        //info
-        this.startX = startX;
-        this.startY = startY;
-        this.endX = endX;
-        this.endY = endY;
-        //path
-        this.pathNumber = addressList.size();
-        this.passedPath = new ArrayList<>();
-        this.remainPath = new ArrayList<>(addressList);
-        //overlay
-        overlayPassedPath = null;
-        overlayRemainPath = null;
-        //overlay reset
-        refreshRemainPath();
+    public CourseOverseer(MapDrawer mapDrawer){
+        this.mapDrawer=mapDrawer;
+        this.currentLocation=null;
+        this.markers=null;
+        this.markersLocationData=null;
     }
+    //User Location
+    ScopeDotLocation currentLocation;
+    //Map Data
+    List<Object> markers;
+    List<ScopeDotAddress> markersLocationData;
 
-    public void refresh(ScopeDotAddress currentLocation) {
-        boolean changed = oversightLocation(currentLocation);
-        if (changed) {
-            refreshPassedPath();
-            refreshRemainPath();
-            checkMiddleGoal();
+    public void startOverseer(List<ScopeDotAddress> course, ScopeDotLocation currentLocation){
+        this.currentLocation=currentLocation;
+        for(ScopeDotAddress address:course){
+            markers.add(this.mapDrawer.drawOverlayMarker(address));
+            markersLocationData.add(address);
         }
+        //draw first course
+        this.mapDrawer.drawCourse(course);
     }
+    public void refreshOversight(double latitude, double longtitude){
+        this.currentLocation.refreshLocation(latitude,longtitude);
 
-    public void refresh(double longitude, double latitude) {
-        refresh(new ScopeDotAddress(startX, startY, endX, endY, longitude, latitude));
     }
-
-    private boolean oversightLocation(ScopeDotAddress currentLocation) {
-        if (remainPath.size() == 0) {
-            return false;
-        }
-        ScopeDotAddress currentGoal = remainPath.get(0);
-        //0.0001이하의 거리를 가지면 완료된 코스지점으로 바뀐다.
-        if (currentGoal.getCost(currentLocation) <= 0.0001) {
-            passedPath.add(currentGoal);
-            remainPath.remove(currentGoal);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void refreshPassedPath() {
-        if (overlayPassedPath != null) {
-            this.mapDrawer.clearDraw(overlayPassedPath);
-            overlayPassedPath = null;
-        }
-        if (passedPath.size() >= 2) {
-            this.mapDrawer.drawPassedPath(passedPath);
-        }
-    }
-
-    private void refreshRemainPath() {
-        if (overlayRemainPath != null) {
-            this.mapDrawer.clearDraw(overlayRemainPath);
-            overlayRemainPath = null;
-        }
-        if (remainPath.size() >= 2) {
-            this.mapDrawer.drawRemainPath(remainPath);
-        }
-    }
-
-    private void checkMiddleGoal() {
-        int passedPathNumber = passedPath.size();
-        if (passedPathNumber == (pathNumber * 1) / 4) {
-            //sound 1/4
-        }
-        if (passedPathNumber == (pathNumber * 2) / 4) {
-            //sound 2/4
-        }
-        if (passedPathNumber == (pathNumber * 3) / 4) {
-            //sound 3/4
-        }
-        if (passedPathNumber == (pathNumber * 4) / 4) {
-            //sound 4/4
-        }
-    }
+    //course를 db로 분리해서 쓰는 것이 결과물
+    //course와 현재데이터는 지속적으로 DB에 갱신하고 이를 사용하는 식으로 구현되어야 한다.
+    //마커, 코스데이터, 그려진 코스 데이터
+    //현재까지 지나갔던 데이터까지 저장하게 된다. 대략 15미터 간격으로
+    //DB 만들기
 }
