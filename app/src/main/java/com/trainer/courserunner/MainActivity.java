4327885dd93608b1fdb7a82a3f6a0f3583c38c09
Kim, Hyunjung <hjk0385@gmail.com>
2021-04-22 07:53:26 +0900

    second commit

--- app/src/main/java/com/trainer/courserunner/MainActivity.java
+++ app/src/main/java/com/trainer/courserunner/MainActivity.java
@@ -2,7 +2,13 @@ package com.trainer.courserunner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button imageButton = (Button) findViewById((R.id.running_btn));
+        imageButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), RunningActivity.class);
+            startActivity(intent);
+        });
+        Button imageButton2 = (Button) findViewById((R.id.result_btn));
+        imageButton2.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            startActivity(intent);
+        });
+        Button imageButton3 = (Button) findViewById((R.id.mission_btn));
+        imageButton3.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MissionActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_btn2: //사용자 계정 관리 이동
+                Intent homeIntent = new Intent(this, UserSetActivity.class);
+                startActivity(homeIntent);
+                return true;
+            case R.id.action_btn3: //효과음 설정 이동
+                Intent homeIntent2 = new Intent(this, SoundSetActivity.class);
+                startActivity(homeIntent2);
+                return true;
+            case R.id.action_btn4: //공지사항 및 QnA 카페 이동
+                Intent home3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cafe.naver.com/drawingrunners"));
+                startActivity(home3Intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void playBtn() {
     }
 }
--- app/src/main/java/com/trainer/courserunner/RunningActivity.java
+++ app/src/main/java/com/trainer/courserunner/RunningActivity.java
@@ -2,7 +2,9 @@ package com.trainer.courserunner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RunningActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class RunningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
+
+        Button imageButton = (Button) findViewById((R.id.nornal_running_btn));
+        imageButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), NormalRunningActivity.class);
+            startActivity(intent);
+        });
+
+        Button imageButton1 = (Button) findViewById((R.id.project_running_btn));
+        imageButton1.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ProjectRunningActivity.class);
+            startActivity(intent);
+        });
+
+        Button imageButton2 = (Button) findViewById((R.id.Sketchbook_running_btn));
+        imageButton2.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MissionActivity.class);
+            startActivity(intent);
+        });
     }
 }
