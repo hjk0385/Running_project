5920e01db4cf1ff3f4789348d4b195385c0c5b8f
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-04-23 13:09:12 +0900

    Finish

--- app/src/main/java/com/trainer/courserunner/NormalRunningActivity.java
+++ app/src/main/java/com/trainer/courserunner/NormalRunningActivity.java
@@ -2,7 +2,12 @@ package com.trainer.courserunner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.trainer.courserunner.course.CourseGuideActivity;
 
 public class NormalRunningActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class NormalRunningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_running);
+
+        //버튼 리스너 등록
+        Button km2_btn=(Button) findViewById(R.id.km2_btn);
+        km2_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //엑티비티 인텐드
+                Intent intent =new Intent(getBaseContext(), CourseGuideActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
