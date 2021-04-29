f206df163e65fc3f331550bc5f132239acd82d29
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-04-29 22:00:28 +0900

    context

--- app/src/main/java/com/trainer/courserunner/rooms/AppDatabaseLoader.java
+++ app/src/main/java/com/trainer/courserunner/rooms/AppDatabaseLoader.java
@@ -6,17 +6,17 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.trainer.courserunner.MyApplication;
 
 public class AppDatabaseLoader extends Application {
     static private AppDatabase appDatabase = null;
-
     static public AppDatabase getAppDatabase() {
-        if (appDatabase == null) {
-            Context applicationContext = MyApplication.ApplicationContext();
-            appDatabase = Room.databaseBuilder(applicationContext, AppDatabase.class,
-                    "testdatabase114").build();
-        }
         return appDatabase;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        //앱이 처음으로 시작될때 호출되는 메소드
+        appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,
+                "testdatabase114").build();
     }
 }
