fb9f6c16b2057084c81fed0a2f961083b586d8e5
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-04-29 21:16:10 +0900

    commit

--- app/src/main/java/com/trainer/courserunner/rooms/AppDatabase.java
+++ app/src/main/java/com/trainer/courserunner/rooms/AppDatabase.java
@@ -2,12 +2,12 @@ package com.trainer.courserunner.rooms;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
-@Database(entities = {Course.class, CourseFlag.class, MapFlag.class,
-        UserCourseInfo.class,
-        UserLocationRecord.class, UserMapFlag.class}, version = 1, exportSchema = false)
+@Database(entities = {Course.class, CourseFlag.class, CourseMode.class}, version = 1, exportSchema = false)
+@TypeConverters({DateConverters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CourseDao courseDao();
-
-    public abstract UserCourseDao userCourseDao();
+    public abstract CourseFlagDao courseFlagDao();
+    public abstract CourseModeDao courseModeDao();
 }
