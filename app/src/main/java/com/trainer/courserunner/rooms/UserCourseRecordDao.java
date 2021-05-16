0069d01e178e25fa27a741d8d337d6358980df7a
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-05-16 16:03:42 +0900

    DB정렬

--- app/src/main/java/com/trainer/courserunner/rooms/UserCourseRecordDao.java
+++ app/src/main/java/com/trainer/courserunner/rooms/UserCourseRecordDao.java
@@ -8,7 +8,7 @@ public interface UserCourseRecordDao extends BaseDao<UserCourseRecord> {
     @Query("SELECT IFNULL(MAX(user_course_record_id),0)+1 FROM user_course_record WHERE user_course_id=:user_course_id")
     Long getNextUserLocationOrder(Long user_course_id);
 
-    @Query("SELECT * FROM user_course_record WHERE user_course_id=:user_course_id")
+    @Query("SELECT * FROM user_course_record WHERE user_course_id=:user_course_id ORDER BY user_course_record_id")
     UserCourseRecord[] getUserLocationRecords(Long user_course_id);
 
     @Query("SELECT * FROM user_course_record WHERE user_course_id=:user_course_id limit :limit")
