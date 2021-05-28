cfdf0f10f36a2d95656bf81870022797d3282ad3
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-05-28 17:22:46 +0900

    commit

--- app/src/main/java/com/trainer/courserunner/Application/bandsupport/GalaxyFitFunctionSupporter.java
+++ app/src/main/java/com/trainer/courserunner/Application/bandsupport/GalaxyFitFunctionSupporter.java
@@ -1,8 +1,34 @@
 package com.trainer.courserunner.Application.bandsupport;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.Menu;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
+import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthData;
+import com.samsung.android.sdk.healthdata.HealthDataObserver;
+import com.samsung.android.sdk.healthdata.HealthDataResolver;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 public class GalaxyFitFunctionSupporter extends FunctionSupporter implements BandFunctionSupport{
+
     public GalaxyFitFunctionSupporter(Context context) {
         super(context);
     }
