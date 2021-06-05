ab665b45e4c7d5393853880449c3ee90f146c2be
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-06-05 19:50:36 +0900

    commit

--- app/src/main/java/com/trainer/courserunner/Application/rooms/UserCourseAnalyzer.java
+++ app/src/main/java/com/trainer/courserunner/Application/rooms/UserCourseAnalyzer.java
@@ -1,4 +1,26 @@
 package com.trainer.courserunner.Application.rooms;
 
+import com.trainer.courserunner.geo.DistanceConverter;
+
 public class UserCourseAnalyzer {
+    //m기준
+    public double getDistance(long userCourseId){
+        UserCourseDetail[] userCourseDetails= AppDatabaseConnector.getAppDatabaseConnection()
+                .userCourseDetailDao()
+                .getUserCourseDetailWhereUserCourseIdOrder(userCourseId);
+        if(userCourseDetails.length==0){
+            return -1;
+        }
+
+        double fullDistance=0;
+        for(int i=1;i<userCourseDetails.length;i++){
+            double latitude1=userCourseDetails[i-1].userCourseRecordLatitude;
+            double longitude1=userCourseDetails[i-1].userCourseRecordLongitude;
+            double latitude2=userCourseDetails[i].userCourseRecordLatitude;
+            double longitude2=userCourseDetails[i].userCourseRecordLongitude;
+            fullDistance+=DistanceConverter.getDistance(latitude1,longitude1,latitude2,longitude2);
+        }
+        return fullDistance;
+    }
+
 }
--- app/src/main/java/com/trainer/courserunner/Application/rooms/UserCourseDetail.java
+++ app/src/main/java/com/trainer/courserunner/Application/rooms/UserCourseDetail.java
@@ -1,13 +1,17 @@
 package com.trainer.courserunner.Application.rooms;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.DatabaseView;
 
 import java.util.Date;
 
-@DatabaseView("SELECT user_course.user_course_id, user_course.course_id, user_course.course_mode_id, user_course.user_course_name, " +
-        " user_course_record.user_course_record_id, user_course_record.user_course_record_date" +
-        " FROM user_course JOIN user_course_record ON user_course.user_course_id = user_course_record.user_course_id ")
+@DatabaseView("SELECT user_course.user_course_id, user_course.user_course_name, " +
+        " user_course.course_mode_id, user_course.course_id, " +
+        " user_course_record_id, user_course_record_color, " +
+        " user_course_record_latitude, user_course_record_longitude," +
+        " user_course_record_date" +
+        " FROM user_course JOIN user_course_record USING(user_course_id)")
 public class UserCourseDetail {
     @ColumnInfo(name = "user_course_id")
     public Long userCourseId;
@@ -17,10 +21,14 @@ public class UserCourseDetail {
     public Long courseModeId;
     @ColumnInfo(name = "course_id")
     public Long courseId;
-
-
     @ColumnInfo(name = "user_course_record_id")
     public Long userCourseRecordId;
+    @ColumnInfo(name = "user_course_record_color")
+    public Integer userCourseRecordColor;
+    @ColumnInfo(name = "user_course_record_latitude")
+    public Double userCourseRecordLatitude;
+    @ColumnInfo(name = "user_course_record_longitude")
+    public Double userCourseRecordLongitude;
     @ColumnInfo(name = "user_course_record_date")
     public Date userCourseRecordDate;
 }
