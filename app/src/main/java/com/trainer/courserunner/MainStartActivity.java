8e3af1dc61eadd0b8ea7a125c302b69026317c18
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-06-05 20:09:57 +0900

    commit

--- app/src/main/java/com/trainer/courserunner/MainStartActivity.java
+++ app/src/main/java/com/trainer/courserunner/MainStartActivity.java
@@ -3,7 +3,11 @@ package com.trainer.courserunner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +21,8 @@ public class MainStartActivity extends AppCompatActivity {
     private Button mBtnNewStart;
     private Button mBtnUserLog;
     private Button mBtnLoad;
-    private Button mBtnSetting;
+    private Button mBtnExerciseInfo;
+    private Button mBtnMission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,8 @@ public class MainStartActivity extends AppCompatActivity {
         mBtnNewStart=findViewById(R.id.btn_newstart);
         mBtnLoad=findViewById(R.id.btn_load);
         mBtnUserLog=findViewById(R.id.btn_userlog);
-        mBtnSetting=findViewById(R.id.btn_setting);
+        mBtnExerciseInfo=findViewById(R.id.btn_exersicecal);
+        mBtnMission=findViewById(R.id.btn_mission);
 
         mBtnLoginLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,12 +64,43 @@ public class MainStartActivity extends AppCompatActivity {
             }
         });
 
-        mBtnSetting.setOnClickListener(new View.OnClickListener() {
+        mBtnExerciseInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),SettingActivity.class));
+                startActivity(new Intent(getApplicationContext(),ExerciseResultsActivity.class));
             }
         });
-
+        mBtnMission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MissionActivity.class));
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_mainstart, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.menu_noticesite:
+                Intent home3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cafe.naver.com/drawingrunners"));
+                startActivity(home3Intent);
+                return true;
+            case R.id.menu_setting: //설정 이동
+                Intent homeIntent2 = new Intent(this, SettingActivity.class);
+                startActivity(homeIntent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
