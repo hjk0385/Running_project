591835dd5d52db6027565ace1301f34bce5b39c9
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-06-06 15:26:57 +0900

    commit

--- app/src/main/java/com/trainer/courserunner/ResultActivity.java
+++ app/src/main/java/com/trainer/courserunner/ResultActivity.java
@@ -1,14 +1,23 @@
 package com.trainer.courserunner;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
+    private TextView textViewMonthCalorie;
+    private TextView textViewMonthDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        textViewMonthCalorie=findViewById(R.id.textView_month_calorie);
+        textViewMonthDistance=findViewById(R.id.textView_month_distance);
+
+
+
     }
 }
